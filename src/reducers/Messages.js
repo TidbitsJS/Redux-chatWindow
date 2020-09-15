f27878069cbd624c0508d96bce5a0bbfc62f19bc
sujata <gunalesujata@gmail.com>
2020-09-15 23:14:04 +0530

    message reducer

---
+++ src/reducers/Messages.js
@@ -0,0 +1,27 @@
+import { getMessages } from "../staticData";
+import { SEND_MESSAGE } from "../constants/ActionTypes";
+import _ from "lodash";
+
+export default function messages(state = getMessages(10), action) {
+  switch (action.type) {
+    case SEND_MESSAGE:
+      const { message, userId } = action.payload;
+      const allUserMsgs = state[userId];
+      const number = +_.keys(allUserMsgs).pop() + 1;
+
+      return {
+        ...state,
+        [userId]: {
+          ...allUserMsgs,
+          [number]: {
+            number,
+            text: message,
+            is_user_msg: true,
+          },
+        },
+      };
+
+    default:
+      return state;
+  }
+}
