27e954e5a871215c2d86969d88a4dd1d94137707
sujata <gunalesujata@gmail.com>
2020-09-16 11:10:07 +0530

    chatwindow

---
+++ src/containers/ChatWindow/ChatWindow.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import store from "../../store/Store";
+import Chats from "../../components/Chats/Chats";
+import MessageInput from "../MessageInput/MessageInput";
+import _ from "lodash";
+import "./ChatWindow.css";
+
+const ChatWindow = ({ activeUserId }) => {
+  const state = store.getState();
+  const activeUser = state.contacts[activeUserId];
+  const activeMsgs = state.messages[activeUserId];
+  const { typing } = state;
+
+  return (
+    <div className="ChatWindow">
+      <Header user={activeUser} />
+      <Chats messages={_.values(activeMsgs)} />
+      <MessageInput value={typing} />
+    </div>
+  );
+};
+
+export default ChatWindow;
