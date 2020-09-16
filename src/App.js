a67c96aeea7ac60d43d902944b64227c3e0b7d5d
sujata <gunalesujata@gmail.com>
2020-09-16 13:25:38 +0530

    app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import _ from "lodash";
+import store from "./store/Store";
+import Slidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
 
 function App() {
-  return <div className="App"></div>;
+  const { contacts, user, activeUserId } = store.getState();
+
+  return (
+    <div className="App">
+      <Slidebar contacts={_.values(contacts)} />
+      <Main user={user} activeUserId={activeUserId} />
+    </div>
+  );
 }
 
 export default App;
